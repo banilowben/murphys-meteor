687cc2787ab4acc8c6a961bb3e63041aee317217
Ben <156398965+banilowben@users.noreply.github.com>
2024-03-05 18:54:24 -1000

    Ported islandsnow into Meteor
    Done with main part

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Middle from '../components/Middle';
+
+const Murphys = () => (
+  <div className="d-flex flex-column min-vh-100">
+    <Middle />
+  </div>
+);
+
+export default Murphys;
